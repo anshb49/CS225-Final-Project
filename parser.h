f22f4f0b0637757373e2317eab29ea74211df0b2
anshb49 <anshbhalla1@gmail.com>
2020-11-29 14:14:09 -0800

    created main, makefile, and file to vector

---
+++ parser.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+
+
+std::string file_to_string(const std::string & filename);
+
+std::vector<std::string> file_to_vector(const std::string & filename);
