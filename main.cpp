46d10c721b5f39ad90abae936156fb85291a7234
anshb49 <anshbhalla1@gmail.com>
2020-11-29 14:39:14 -0800

    Parsed ENGB data file into vector

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include "parser.h"
 
+
 int main(int argc, const char * argv[]) {
 	std::cout << "Filename: " << argv[1] << std::endl;
 	
@@ -11,10 +12,37 @@ int main(int argc, const char * argv[]) {
 	
 	// convert file to vector representation
     //std::vector<string> id_vector;
+	std::vector<std::string> ID_vector;
+	std::vector<std::string> days_vector;
+	std::vector<std::string> mature_vector;
+	std::vector<std::string> views_vector;
+	std::vector<std::string> partner_vector;
+	std::vector<std::string> newID_vector;
+
+	std::vector<std::vector<std::string>> data_vector;
 
 	std::cout << "Vector Representation:" << std::endl;
 	std::vector<std::string> vectorRepr = file_to_vector(argv[1]);
+	
 	for (auto word : vectorRepr) {
-		std::cout << word << std::endl;
-	}	
+		int rowCount = 0;
+		int stringCount = 0;
+		std::stringstream s_stream(word);
+
+		std::vector<std::string> row;
+		while (s_stream.good()) {
+			/* code */
+			std::string temp;
+			getline(s_stream, temp, ',');
+			row.push_back(temp);
+			stringCount++;
+		}
+		data_vector.push_back(row);
+	}
+
+	std::cout<<data_vector[15].at(5) << std::endl;
+	std::cout<<data_vector[34].at(5)<< std::endl;
+	std::cout<<data_vector[89].at(5)<< std::endl;
+
+
 }
